d77fc82fa6165f87c065614628adf5e094498c7e
Nat89 <grupaa.nowak@gmail.com>
2019-02-25 11:52:29 +0100

    11:52

--- src/Model/Course.java
+++ src/Model/Course.java
@@ -1,4 +1,60 @@
 package Model;
 
+import java.time.LocalDate;
+
 public class Course {
+    private int id;
+    private  String course_name;
+    private  String course_category;
+    private LocalDate course_date;
+    private  String trener_name;
+
+
+    public Course(int id, String course_name, String course_category, LocalDate course_date, String trener_name) {
+        this.id = id;
+        this.course_name = course_name;
+        this.course_category = course_category;
+        this.course_date = course_date;
+        this.trener_name = trener_name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCourse_name() {
+        return course_name;
+    }
+
+    public void setCourse_name(String course_name) {
+        this.course_name = course_name;
+    }
+
+    public String getCourse_category() {
+        return course_category;
+    }
+
+    public void setCourse_category(String course_category) {
+        this.course_category = course_category;
+    }
+
+    public LocalDate getCourse_date() {
+        return course_date;
+    }
+
+    public void setCourse_date(LocalDate course_date) {
+        this.course_date = course_date;
+    }
+
+    public String getTrener_name() {
+        return trener_name;
+    }
+
+    public void setTrener_name(String trener_name) {
+        this.trener_name = trener_name;
+    }
 }
---
+++ src/controller/CourseController.java
@@ -0,0 +1,91 @@
+package controller;
+
+
+import Model.Course;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.time.LocalDate;
+
+public class CourseController {
+    // ---------------------------------------------------
+
+    // obiekt listy kursow
+    ObservableList<Course> courses = FXCollections.observableArrayList();
+    public static  int id;
+    // ---------------------------------------------------
+
+    @FXML
+    private TableView<Course> tbl_course;
+
+    @FXML
+    private TableColumn<Course, String> col_course_name;
+
+    @FXML
+    private TableColumn<Course, String> col_course_category;
+
+    @FXML
+    private TextField tf_course_name;
+
+    @FXML
+    private TextField tf_course_category;
+
+    @FXML
+    private DatePicker dp_course_date;
+
+    @FXML
+    private TextField tf_trener_name;
+
+    @FXML
+    void addCourse(ActionEvent event) {
+
+        // walidacja danych
+        if(!tf_course_name.getText().equals("") && !tf_course_category.getText().equals("") && dp_course_date.getValue() != null) {
+            String course_name = tf_course_name.getText();
+            String course_category = tf_course_category.getText();
+            LocalDate course_date = dp_course_date.getValue();
+            String trener_name = tf_trener_name.getText();
+            // utworzenie obiektu klasy Course
+            Course c = new Course(++id, course_name, course_category, course_date, trener_name);
+            // wprowadzenie obiektu klasy Course do listy kursów
+            courses.add(c);
+            // odswiezenie TableView
+            insertCoursesIntoTableView();
+            tf_course_name.clear();
+            tf_course_category.clear();
+            dp_course_date.setValue(null);
+            tf_trener_name.clear();
+        } else {
+                System.out.println("Błąd");
+            }
+
+    }
+
+    @FXML
+    void deleteCourse(ActionEvent event) {
+
+    }
+
+    @FXML
+    void getCourse(ActionEvent event) {
+
+    }
+    // metoda do wprowadzanai zawartosci do listy kursow do tabelki
+    private  void insertCoursesIntoTableView() {
+        col_course_name.setCellValueFactory(new PropertyValueFactory<Course, String>("course_name"));
+        col_course_category.setCellValueFactory(new PropertyValueFactory<Course, String>("course_category"));
+        // wprowadzenia danych do tabeli z listy ObservableList\
+        tbl_course.setItems(courses);
+    }
+    // metoda ktroa uruchamia sie po zaladowaniu widoku jako pierwsza
+    public void initialize(){
+        insertCoursesIntoTableView();
+    }
+}
